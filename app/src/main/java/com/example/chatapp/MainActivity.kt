1ccdc7374fd2cd6556cb9d2180d1c0b487b411fe
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2022-12-27 18:30:36 +0100

    Login & Registration backend

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.chatapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.chatapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
@@ -18,6 +22,33 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.signin.setOnClickListener{
+            val email = binding.email.text.toString()
+            val pass = binding.password.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(this, "Succesfully signed in", Toast.LENGTH_SHORT).show()
+                            val intent2 = Intent(this, ChatApp_Activity::class.java)
+                            startActivity(intent2)
+                            finish()
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this, "Empty fields are not allowed !!", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
 
     }
 }
--- app/src/main/java/com/example/chatapp/Register_Activity.kt
+++ app/src/main/java/com/example/chatapp/Register_Activity.kt
@@ -3,11 +3,19 @@ package com.example.chatapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.chatapp.databinding.ActivityMainBinding
 import com.example.chatapp.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Register_Activity : AppCompatActivity() {
     private lateinit var binding:ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
@@ -18,6 +26,35 @@ class Register_Activity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.CreateAccount.setOnClickListener{
+            val email = binding.email.text.toString()
+            val pass = binding.password.text.toString()
+            val username = binding.Username.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty() && username.isNotEmpty()){
+               firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val user = firebaseAuth.currentUser?.uid.toString()
+                            val db = FirebaseFirestore.getInstance()
+                            val usernamemap = hashMapOf(
+                                "Username" to username
+                            )
+                            db.collection("users").document(user).set(usernamemap)
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                           Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG)
+                                    .show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this, "empty fields are not allowed !!", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
