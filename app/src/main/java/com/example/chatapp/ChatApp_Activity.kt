1347434f4ac926cd3af3dfcff971b2e887d82d09
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2022-12-27 22:12:48 +0100

    Actionbar & sign out

--- app/src/main/java/com/example/chatapp/ChatApp_Activity.kt
+++ app/src/main/java/com/example/chatapp/ChatApp_Activity.kt
@@ -1,11 +1,32 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.chatapp.databinding.ActivityChatAppBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ChatApp_Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatAppBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat_app)
+        binding = ActivityChatAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Firebase.auth.signOut()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+        return super.onContextItemSelected(item)
     }
 }
