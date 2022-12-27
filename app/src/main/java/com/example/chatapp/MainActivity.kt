bc402f3f050c340508e3146210ef931aa9958272
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2022-12-27 15:54:52 +0100

    Connecting views

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ToRegister.setOnClickListener{
+            val intent = Intent(this, Register_Activity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/chatapp/Register_Activity.kt
+++ app/src/main/java/com/example/chatapp/Register_Activity.kt
@@ -1,11 +1,23 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapp.databinding.ActivityMainBinding
+import com.example.chatapp.databinding.ActivityRegisterBinding
 
 class Register_Activity : AppCompatActivity() {
+    private lateinit var binding:ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ToLogin.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
