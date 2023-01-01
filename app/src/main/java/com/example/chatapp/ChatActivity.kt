184a7ecaea4c6963853c7e65369f04115fc65c38
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-01-01 22:22:21 +0100

    passing data from users activity to chat activity

--- app/src/main/java/com/example/chatapp/ChatActivity.kt
+++ app/src/main/java/com/example/chatapp/ChatActivity.kt
@@ -1,12 +1,23 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapp.databinding.ActivityChatAppBinding
+import com.example.chatapp.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = Intent()
+        val name = intent.getStringExtra("name")
+        val uid = intent.getStringExtra("uid")
+
+        supportActionBar?.title = name
 
     }
 }
--- app/src/main/java/com/example/chatapp/UserAdaper.kt
+++ app/src/main/java/com/example/chatapp/UserAdaper.kt
@@ -1,11 +1,13 @@
 package com.example.chatapp
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class UserAdaper(val context: Context, val userList: ArrayList<User>): RecyclerView.Adapter<UserAdaper.UserViewholder>() {
 
@@ -18,6 +20,13 @@ class UserAdaper(val context: Context, val userList: ArrayList<User>): RecyclerV
         val currentUser = userList[position]
         holder.username.text = currentUser.username
         holder.id.text = currentUser.uid
+
+        holder.itemView.setOnClickListener{
+            val intent = Intent(context, ChatActivity::class.java)
+            intent.putExtra("name", currentUser.username)
+            intent.putExtra("uid", FirebaseAuth.getInstance().currentUser?.uid)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
